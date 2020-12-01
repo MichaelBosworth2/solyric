c97d441ea25c29258441e3f884b25b1d3902d301
Carlos Pérez <carlosperez@Carloss-MacBook-Pro.local>
2020-12-01 17:08:08 -0600

    Add ValueNotifier to listen bottom navigation tab changes

--- lib/app/ui/home/HomeScreen.dart
+++ lib/app/ui/home/HomeScreen.dart
@@ -25,75 +25,82 @@ class _HomeScreenState extends State<HomeScreen> {
     ProfileScreen()
   ];
 
-  var currentScreen = 0;
+  final myNotifier = ValueNotifier(0);
 
   @override
   Widget build(BuildContext context) {
     final controller = SwiperController();
 
-    return Scaffold(
-      appBar: UIHelper.commonAppBar(context,
-          backButton: false,
-          backgroundTransparent: currentScreen == 1 ? true : false),
-      // body: getLayoutScreen(currentScreen, controller),
-      body: Container(
-        decoration: BoxDecoration(
-            color: Colors.black87,
-            image: DecorationImage(
-                image: AssetImage(Resources.BACKGROUND),
-                colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
-                fit: BoxFit.cover)),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
-          child: SafeArea(
-              child: Swiper(
-            itemBuilder: (context, index) => screenItems[index],
-            itemCount: screenItems.length,
-            controller: controller,
-          )),
-        ),
-      ),
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.transparent,
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.grey,
-        unselectedItemColor: Colors.grey,
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(Resources.IC_EXPLORE, color: Colors.grey),
-            title: Text(Resources.EXPLORE),
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(Resources.IC_SEARCH, color: Colors.grey),
-            title: Text(Resources.SEARCH),
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(Resources.IC_CHORDS, color: Colors.grey),
-            title: Text(Resources.CHORDS),
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(Resources.IC_ACCOUNT, color: Colors.grey),
-            title: Text(Resources.ACCOUNT),
-          )
-        ],
-        onTap: (position) {
-          _onItemTapped(position, controller);
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        onPressed: () => Navigator.pushNamed(context, RouteNames.HOME),
-        child: SvgPicture.asset(Resources.IC_PLUS),
-      ),
-    );
+    return ValueListenableBuilder(
+        valueListenable: myNotifier,
+        builder: (context, value, child) {
+          return Scaffold(
+            appBar: UIHelper.commonAppBar(context,
+                backButton: false,
+                backgroundTransparent: value == 1 ? true : false),
+            body: Container(
+              decoration: BoxDecoration(
+                  color: Colors.black87,
+                  image: DecorationImage(
+                      image: AssetImage(Resources.BACKGROUND),
+                      colorFilter:
+                          ColorFilter.mode(Colors.black54, BlendMode.darken),
+                      fit: BoxFit.cover)),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
+                child: SafeArea(
+                    child: Swiper(
+                  itemBuilder: (context, index) => screenItems[index],
+                  itemCount: screenItems.length,
+                  controller: controller,
+                )),
+              ),
+            ),
+            extendBodyBehindAppBar: true,
+            backgroundColor: Colors.transparent,
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: Colors.grey,
+              unselectedItemColor: Colors.grey,
+              items: [
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(Resources.IC_EXPLORE,
+                      color: Colors.grey),
+                  title: Text(Resources.EXPLORE),
+                ),
+                BottomNavigationBarItem(
+                  icon:
+                      SvgPicture.asset(Resources.IC_SEARCH, color: Colors.grey),
+                  title: Text(Resources.SEARCH),
+                ),
+                BottomNavigationBarItem(
+                  icon:
+                      SvgPicture.asset(Resources.IC_CHORDS, color: Colors.grey),
+                  title: Text(Resources.CHORDS),
+                ),
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(Resources.IC_ACCOUNT,
+                      color: Colors.grey),
+                  title: Text(Resources.ACCOUNT),
+                )
+              ],
+              onTap: (position) {
+                _onItemTapped(position, controller);
+              },
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: () => Navigator.pushNamed(context, RouteNames.HOME),
+              child: SvgPicture.asset(Resources.IC_PLUS),
+            ),
+          );
+        });
   }
 
   void _onItemTapped(int index, SwiperController controller) {
-    // setState(() {
-    //   currentScreen = index;
-    // });
+    myNotifier.value = index;
     controller.move(index, animation: true);
   }
 }
